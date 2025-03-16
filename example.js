57aeccffc2afd032c4f7f92273ec1eceac5e0069
David J. Bradshaw <dave@bradshaw.net>
2025-03-16 11:26:31 +0000

    Change API to use console.event()

--- example.js
+++ example.js
@@ -2,8 +2,19 @@ import { createDeferConsole, createGroupConsole } from './lib/index'
 
 window.createDeferConsole = createDeferConsole
 
-const deferConsole = createDeferConsole({ title: 'Defer Console' })
-const groupConsole = createGroupConsole({ title: 'Group Console' })
+const deferConsole = createDeferConsole({ title: 'Defer', defaultEvent: 'defer' })
+const groupConsole = createGroupConsole({ title: 'Group' })
 
 deferConsole.log('This will be logged after the group console')
+deferConsole.event('event')
+
+// groupConsole.event('event')
 groupConsole.log('This will be logged in a group')
+groupConsole.endAutoGroup()
+
+groupConsole.event('secondEvent')
+groupConsole.log('log 1')
+groupConsole.log('log 2')
+groupConsole.endAutoGroup()
+
+groupConsole.log('log 3')
