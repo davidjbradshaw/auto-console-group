4ffa91f2cd4bf30deb5ac76f9f770e49dea94488
David J. Bradshaw <dave@bradshaw.net>
2025-03-24 12:44:40 +0000

    Update imports

--- lib/index.ts
+++ lib/index.ts
@@ -1,12 +1,4 @@
-import createConsoleGroup from 'auto-console-group'
-
-const BOLD = 'font-weight: bold;'
-const BLUE = 'color: #135CD2;'
-const BLUE_LIGHT = 'color: #A9C7FB;'
-
-const isDarkModeEnabled = (): boolean => window?.matchMedia('(prefers-color-scheme: dark)').matches
-
-const HIGHLIGHT = isDarkModeEnabled() ? BLUE_LIGHT : BLUE
+import createConsoleGroup, { BOLD, HIGHLIGHT } from 'auto-console-group'
 
 const consoleGroup = createConsoleGroup({
   label: 'auto-group-console',
