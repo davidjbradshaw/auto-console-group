61789b35f602d499a161aea17dfd44680d632ca0
David J. Bradshaw <dave@bradshaw.net>
2025-03-26 10:10:19 +0000

    Fix isDarkModeEnabled()

--- lib/colours.ts
+++ lib/colours.ts
@@ -3,7 +3,7 @@ import {
 } from './consts'
 
 const isDarkModeEnabled = (): boolean =>
-  typeof window !== 'undefined' ? window.matchMedia('(prefers-color-scheme: dark)').matches : false
+  typeof window === 'undefined' ? false : window?.matchMedia('(prefers-color-scheme: dark)').matches
 
 export const HIGHLIGHT = isDarkModeEnabled() ? BLUE_LIGHT : BLUE
 export const FOREGROUND = isDarkModeEnabled() ? WHITE : BLACK
