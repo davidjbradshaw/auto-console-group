f61158b80c0061b5c88f5a958893b1ade5d9080c
David J. Bradshaw <dave@bradshaw.net>
2025-04-01 18:26:30 +0100

    Fix check window.matchMedia is a function for vitest

--- lib/colours.ts
+++ lib/colours.ts
@@ -3,7 +3,9 @@ import {
 } from './consts'
 
 const isDarkModeEnabled = (): boolean =>
-  typeof window === 'undefined' ? false : window?.matchMedia('(prefers-color-scheme: dark)').matches
+  typeof window === 'undefined' || typeof window.matchMedia !== 'function'
+    ? false
+    : window.matchMedia('(prefers-color-scheme: dark)').matches
 
 export const HIGHLIGHT = isDarkModeEnabled() ? BLUE_LIGHT : BLUE
 export const FOREGROUND = isDarkModeEnabled() ? WHITE : BLACK
