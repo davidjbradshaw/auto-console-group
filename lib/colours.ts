b8703202be6710afb45c65270bf6c50191cf3500
David J. Bradshaw <dave@bradshaw.net>
2025-04-01 18:31:21 +0100

    Tidy

--- lib/colours.ts
+++ lib/colours.ts
@@ -2,10 +2,9 @@ import {
   BLACK, BLUE, BLUE_LIGHT, WHITE,
 } from './consts'
 
-const isDarkModeEnabled = (): boolean =>
-  typeof window === 'undefined' || typeof window.matchMedia !== 'function'
-    ? false
-    : window.matchMedia('(prefers-color-scheme: dark)').matches
+const isDarkModeEnabled = typeof window === 'undefined' || typeof window.matchMedia !== 'function'
+  ? false
+  : window.matchMedia('(prefers-color-scheme: dark)').matches
 
-export const HIGHLIGHT = isDarkModeEnabled() ? BLUE_LIGHT : BLUE
-export const FOREGROUND = isDarkModeEnabled() ? WHITE : BLACK
+export const HIGHLIGHT = isDarkModeEnabled ? BLUE_LIGHT : BLUE
+export const FOREGROUND = isDarkModeEnabled ? WHITE : BLACK
