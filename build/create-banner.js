ad2b9365a92b3c4c385c35a3f8660534bfe5c129
David J. Bradshaw <dave@bradshaw.net>
2025-04-02 16:55:46 +0100

    Fix UMD version and create downloadable JS version

---
+++ build/create-banner.js
@@ -0,0 +1,19 @@
+import pkg from '../package.json' with { type: 'json' }
+
+const date = new Date()
+const year = date.getFullYear()
+// const today = date.toISOString().split('T')[0]
+
+export default () => `/*!
+ *  @module      auto-console-group v${pkg.version}
+ *
+ *  @description Automagically group console logs in the browser console.
+ *
+ *  @author      ${pkg.author.name} <${pkg.author.email}>
+ *  @see         {@link ${pkg.homepage}}
+ *  @license     ${pkg.license}
+ *
+ *  @copyright  (c) ${year}, ${pkg.author.name}. All rights reserved.
+ */
+
+`
--- lib/index.ts
+++ lib/index.ts
@@ -1,11 +1,3 @@
-/*!
- *  @module      auto-console-group
- *  @description Automagically group console messages
- *  @author      David J. Bradshaw
- *  @copyright  (c)2025 David J. Bradshaw. All rights reserved.
- *  @license     MIT
- */
-
 import createConsoleGroup from './auto-console-group'
 
 export { FOREGROUND, HIGHLIGHT } from './colours'
